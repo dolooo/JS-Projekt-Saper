d290e65d88c482e4583abc72b967dc2ab45b0899
dolooo <72699696+dolooo@users.noreply.github.com>
2021-06-06 17:42:28 +0200

    Delete RunGame.py

--- RunGame.py
+++
@@ -1,8 +0,0 @@
-from MySaper import MySaper
-from MainWindow import MainWindow
-
-
-if __name__ == '__main__':
-    gameSettings = MySaper()
-    gameWindow = MainWindow(gameSettings)
-    gameWindow.windowLoop()
